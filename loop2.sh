d30f1cfb20da56cc66e70bf34008de2cec112749
PrasadPalange <prasadapalange@gmail.com>
2026-03-12 13:53:45 +0000

    changes_in_loop2

--- loop2.sh
+++ loop2.sh
@@ -2,6 +2,8 @@
 #!/bin/bash
 
 a=1
+totaleven=0
+totalodd=0
 echo "Enter the last number till want to check even numbers list : "
 read -r num
 
@@ -9,9 +11,16 @@ while [[ $a -le $num ]]
 do
     if [[ $((a % 2)) -eq 0 ]]; then
         echo "$a is even number"
+        totaleven=$((totaleven + 1))
+        b=$((b + 1))
+
     else
         echo "$a is odd number"
+        totalodd=$((totalodd + 1))
+        c=$((c + 1))
     fi
     a=$((a + 1))
-
 done
+echo "Total even numbers = $totaleven"
+echo "Total odd numbers = $totalodd"
+
